215c8c15c86afbf7a0a310a9d96496a5f7d5a184
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-01-23 23:24:04 +0200

    feat: add appConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { appConfig } from './config/app.config';
+import { appConfigSchema } from './config/config.types';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
+      validationSchema: appConfigSchema,
+      validationOptions: {
+        abortEarly: true,
+      },
     }),
     TasksModule,
   ],
--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,5 +1,10 @@
+import * as Joi from 'joi';
 import { AppConfig } from './app.config';
 
 export interface ConfigType {
   app: AppConfig;
 }
+
+export const appConfigSchema = Joi.object({
+  APP_MESSAGE_PREFIX: Joi.string(),
+});
