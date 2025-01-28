731d593bf45ee42603d414095b793da35594b71b
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2025-01-28 02:48:40 +0200

    feat: add few e2e test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { TestSetup } from 'src/test/utils/test.setup';
 import * as request from 'supertest';
+import { TestSetup } from 'test/utils/test.setup';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -33,5 +33,29 @@ describe('AppController (e2e)', () => {
         expect(res.body.name).toBe(testUser.name);
         expect(res.body).not.toHaveProperty('password');
       });
+  });
+
+  it('/auth/register (POST) - duplicate email', async () => {
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser);
+
+    return await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser)
+      .expect(409);
+  });
+
+  it('/auth/login (POST)', async () => {
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser);
+
+    const response = await request(testSetup.app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: testUser.email, password: testUser.password });
+
+    expect(response.status).toBe(201);
+    expect(response.body.accessToken).toBeDefined();
   });
 });
